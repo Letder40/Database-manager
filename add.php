84b00c51402e5b48868fa03170ee4e0fc4d648c9
Letder40 <adrian04rv@gmail.com>
2023-02-26 19:59:55 +0100

    now the data is dinamicaly added

--- add.php
+++ add.php
@@ -2,21 +2,38 @@
 if($_SERVER["REQUEST_METHOD"] == "POST"){
     include "includes/db.php";
     $table = $_POST["table"];
+    $first = true;
+    $id = 0;
 
     if($table != "administrators"){
        
         foreach ($_POST as $column => $value){
+            
             if($column == "table"){
                 continue;
             }
 
-            $stmt = $conn->prepare("INSERT INTO $table ($column) VALUES (:To_insert)");
-            $stmt->bindParam(':To_insert', $value);
+            if($first){
+                $stmt = $conn->prepare("INSERT INTO $table ($column) VALUES (:To_insert)");
+                $stmt->bindParam(':To_insert', $value);
+                $stmt->execute();
+                $first = false;
+                $id = $conn->lastInsertId();
+                continue;
+            }
+            try{
+                $stmt = $conn->prepare("UPDATE $table SET $column = :To_insert WHERE id = :id");
+                $stmt->bindParam(":To_insert", $value);
+                $stmt->bindParam(":id", $id);
+                $stmt->execute();
+            }catch (PDOException $e) {
+                die("Connection failed: " . $e->getMessage());
+                echo "You need to declare an id with AUTO INCREMENT in your table!!!\n";
+            }
         }
-        $stmt->execute();
     
     }    
 
-    header('Location: home.php');
+        header('Location: home.php');
 
 }
